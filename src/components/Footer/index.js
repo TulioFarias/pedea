3fb361b4c2d92624ee28653268a8726ca6dd5b86
TulioFarias <tuliott10@gmail.com>
2023-11-21 12:16:38 -0300

    update footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import '../../sass/footer.scss/footer.scss'
+import { Container } from 'react-bootstrap'
+
+function Footer() {
+  return (
+    <Container fluid className=" my-div-footer">
+      <p className="footer-text">
+        Dados do mapa © 2023 Governo do Estado do Ceará. Todos os direitos
+        reservados.
+      </p>
+
+      <span className="footer-divider">|</span>
+
+      <a
+        href="chrome-extension://efaidnbmnnnibpcajpcglclefindmkaj/https://pedea.sema.ce.gov.br/portal/termos_e_condicoes_de_uso_pedea.pdf"
+        target="_blank"
+        className="footer-link"
+      >
+        Termos e Condições de Uso
+      </a>
+
+      <span className="footer-divider">|</span>
+
+      <a
+        href="chrome-extension://efaidnbmnnnibpcajpcglclefindmkaj/https://pedea.sema.ce.gov.br/portal/aviso_privacidade_pedea.pdf"
+        target="_blank"
+        className="footer-link"
+      >
+        Aviso de Privacidade
+      </a>
+    </Container>
+  )
+}
+
+export default Footer
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
+import Footer from '../../components/Footer'
 import Header from '../../components/Header'
 import NavButtonsLeft from '../../components/NavButtonsLeft'
-
 function Home() {
   return (
     <div>
@@ -10,6 +10,8 @@ function Home() {
       <div>
         <NavButtonsLeft />
       </div>
+
+      <Footer />
     </div>
   )
 }
