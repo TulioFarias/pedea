c47d252bd9611914995c2f0c13c38bf0f04b51b3
TulioFarias <tuliott10@gmail.com>
2024-03-27 14:27:47 -0300

    update fail api

--- src/components/AnimatesRoutes/private-route.js
+++ src/components/AnimatesRoutes/private-route.js
@@ -4,11 +4,34 @@ import { Navigate } from 'react-router-dom'
 
 import Admin from '../../containers/Admin'
 import api from '../../services/api'
+import { logout } from '../../utils/redux/user/actions'
 
 export const Private = () => {
+  const [user, setUser] = useState([])
+  const userData = useSelector(state => state.userInfoSlice.infoUser)
+  const loggedInUserId = userData ? userData.id : null
+  const dispatch = useDispatch()
   const userStorage = localStorage.getItem('pedea-admin: user')
 
-  if (!userStorage) {
+  useEffect(() => {
+    async function loadUserData() {
+      try {
+        const { data } = await api.get('/admin')
+        const loggedInUser = data.find(user => user.id === loggedInUserId)
+
+        if (loggedInUser) {
+          setUser(loggedInUser)
+        }
+      } catch (error) {
+        console.error('Error fetching user data:', error)
+        dispatch(logout())
+      }
+    }
+
+    loadUserData()
+  }, [loggedInUserId, dispatch])
+
+  if (!userStorage || !user) {
     return <Navigate to="/login" />
   }
 
