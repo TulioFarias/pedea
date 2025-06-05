6df8467df836446d3f8677cef19192f54f40c8a3
brunocastrohs <bruno.castrohs@gmail.com>
2025-06-04 22:56:30 -0300

    remove pedea gis template do repositorio

--- src/components/AnimatesRoutes/Animate.js
+++ src/components/AnimatesRoutes/Animate.js
@@ -4,7 +4,6 @@ import { Routes, Route, useLocation } from 'react-router-dom'
 
 import ResetPass from '../../components/login/resetPassword'
 import Admin from '../../containers/Admin'
-import Home from '../../containers/Home'
 import Login from '../../containers/Login'
 import WebPageFAQ from '../../containers/perguntasFrequentes(FAQ)'
 import PageChangeLog from '../../containers/perguntasFrequentes(FAQ)/pageChangeLog'
@@ -18,7 +17,7 @@ function AnimatedRoutes() {
       <AnimatePresence>
         <Fragment>
           <Routes location={location} key={location.pathname}>
-            <Route element={<Home />} path="/" />
+            <Route element={<Login />} path="/" />
             <Route element={<Login />} path="/login" />
             <Route element={<ResetPass />} path="/recuperarsenha" />
             <Route element={<Register />} path="/cadastro" />
